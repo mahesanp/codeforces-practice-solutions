025a1e99c52e02193e955df857d22944ce75e8f2
mahesanp <mahesanp789@gmail.com>
2023-01-04 16:10:38 +0530

    new solutions added 5

---
+++ Playoff.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve();
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("error.txt", "w", stderr);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	cin >> t;
+	
+	while (t--) {
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+void solve()
+{
+	long long int n;
+	cin>>n;
+	cout<<(int)pow(2, n)-1;	
+}
